57a05ca66fa00b1f21c8c4933ea25b19d16ba37e
yxchel <165347232+yxchel@users.noreply.github.com>
2025-07-26 14:51:39 -0500

    Complete 11_temperatureConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  return Math.round(((5/9) * (num - 32)) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  return Math.round(((9/5 * num + 32)) * 10) / 10;
 };
 
 // Do not edit below this line
