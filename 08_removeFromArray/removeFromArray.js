72236d36cb7b7b5daefa80dce240c202a24b0fb8
yxchel <165347232+yxchel@users.noreply.github.com>
2025-07-26 14:24:16 -0500

    Complete 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, num, ...theRest) {
+    for(const rest of theRest) {
+        for(let i = 0; i < arr.length; i++) {
+            if(arr[i] === rest) {
+                arr.splice(i, 1);
+                i--;
+            }
+        }
+    }
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] === num) {arr.splice(i, 1); i--;}
+    }
+    return arr;
 };
 
 // Do not edit below this line
