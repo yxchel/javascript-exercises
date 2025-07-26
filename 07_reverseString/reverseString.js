57d43675087846c0a89e0e5a6aee3da3b8838236
yxchel <165347232+yxchel@users.noreply.github.com>
2025-07-26 00:06:17 -0500

    Complete 07-reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
+const reverseString = function(sentence) {
+    let letters = sentence.split('');
+    let backwards = [];
+    for(let i = letters.length - 1; i >= 0; i--) {
+        backwards.push(letters[i]);
+    }
+    let newSentence = backwards.join('');
+    return newSentence;
 
+
+
+
+    // BETTER SOLUTION
+    // return sentence.split("").reverse().join("");
 };
 
 // Do not edit below this line
