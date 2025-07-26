38a4860fe69e1e4a9bb31a146affd3d41cc1c78a
yxchel <165347232+yxchel@users.noreply.github.com>
2025-07-25 23:58:56 -0500

    Complete 06-repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    let sum = "";
+    if(num < 0) {
+        return 'ERROR';
+    }
+    for(let i = 0; i < num; i++) {
+        sum += word;
+    }
+    return sum;
 };
 
 // Do not edit below this line
