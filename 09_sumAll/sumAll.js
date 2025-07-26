1d901aabb6b8caec00edd10ff0c16fc0d25f44d8
yxchel <165347232+yxchel@users.noreply.github.com>
2025-07-26 14:37:23 -0500

    Complete 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    if(num1 < 0 || num2 < 0 || num1 % 1 != 0 || num2 % 1 != 0 || !Number.isInteger(num1) || 
+    !Number.isInteger(num2)) {return 'ERROR';}
+    if(num1 < num2) {
+        for(let i = num1; i <= num2; i++) {
+            sum += i;
+        }
+    } else {
+        for(let i = num2; i <= num1; i++) {
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
